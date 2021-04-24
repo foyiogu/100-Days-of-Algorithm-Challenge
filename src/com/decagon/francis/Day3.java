f23adec2283cd4979cb8bdd2804868e36ce13dd8
Francis Oyiogu <francis.oyiogu@decagon.dev>
2021-04-25 00:08:42 +0100

    day 2

--- src/com/decagon/francis/Day3.java
+++ src/com/decagon/francis/Day3.java
@@ -1,31 +1,35 @@
 package com.decagon.francis;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day3 {
     public static void main(String[] args) {
-
+        System.out.println(duplicateCount("abBA"));
     }
     //FIRST ONE
     public static int duplicateCount(String text) {
-        int count = 0;
-        String[] arr = text.toLowerCase().split("");
-        Map<String, Integer> map = new HashMap<>();
-        for (String s : arr) {
-            if (map.containsKey(s)) {
-                map.put(s, map.get(s) + 1);
-            } else {
-                map.put(s, 1);
-            }
-        }
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > 1) {
-                count++;
-            }
-        }
-        return count;
+//        int count = 0;
+//        String[] arr = text.toLowerCase().split("");
+//        Map<String, Integer> map = new HashMap<>();
+//        for (String s : arr) {
+//            if (map.containsKey(s)) {
+//                map.put(s, map.get(s) + 1);
+//            } else {
+//                map.put(s, 1);
+//            }
+//        }
+//        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+//            if (entry.getValue() > 1) {
+//                count++;
+//            }
+//        }
+//        return count;
+        return Arrays
+                .stream(text.toLowerCase().split(""))
+                .filter(a-> (Collections.frequency(Arrays.asList(text.toLowerCase().split("")),a))>1)
+                .collect(Collectors.toSet())
+                .size();
     }
 
     //SECOND ONE
