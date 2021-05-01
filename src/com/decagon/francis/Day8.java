cb1fb8e5d49562438c2ccab925cfbb62361ef127
Francis Oyiogu <francis.oyiogu@decagon.dev>
2021-05-01 23:40:25 +0100

    day 8

--- src/com/decagon/francis/Day8.java
+++ src/com/decagon/francis/Day8.java
@@ -9,21 +9,21 @@ public class Day8 {
     public static void main(String[] args) {
         System.out.println(highAndLow("1 2 -9 4 5"));
     }
+
+
+
     public static String highAndLow(String numbers) {
         List<Integer> list = Arrays.stream(numbers.split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        String a = String.valueOf(Collections.max(list));
-        String b = String.valueOf(Collections.min(list));
-        return a+ " "+b;
+        return Collections.max(list) + " " + Collections.min(list);
     }
+
+
+
+
+
+
+
     public static String oddOrEven (int[] array) {
-        int sum = 0;
-        for(int i = 0; i< array.length;i++){
-            sum+=array[i];
-        }
-        if(sum%2 == 0){
-            return "even";
-        }else{
-            return "odd";
-        }
+        return Arrays.stream(array).sum() % 2 ==0 ? "even" : "odd";
     }
 }
